9a76555b48cda5341faeb232c258315132da5270
Pranal PradeepKumar Kuttappan <pranalpk8@gmail.com>
2021-01-04 00:59:02 -0500

    Setting up the HomeScreen. Product Listing and individual Product Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { Container } from "react-bootstrap";
+import Homescreen from "./screens/Homescreen";
+import Productscreen from "./screens/Productscreen";
+
+
 const App = () => {
   return (
-    <>
-    <h1>Welcome to ProShop</h1>
-    </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={Homescreen} exact/>
+          <Route path='/product/:id' component={Productscreen}/>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 
---
+++ frontend/src/screens/Homescreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import products from '../products'
+import {Row, Col} from 'react-bootstrap'
+import Product from '../components/Product'
+
+const Homescreen = () => {
+    return (
+        <>
+        <h1>Latest Products</h1>
+        <Row>
+            {products.map(product=>(
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product}/>
+                </Col>
+            ))}
+        </Row>
+        </>
+    )
+}
+
+export default Homescreen
