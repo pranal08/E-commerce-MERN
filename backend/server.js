c88d19b6b1b808bde2d511c12f80e520c01d4ac9
Pranal PradeepKumar Kuttappan <pranalpk8@gmail.com>
2021-01-09 15:19:52 -0500

    Added custom error handling middleware for API requests

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import connectDB from "./config/db.js"
 import products from "./data/products.js";
 import dotenv from "dotenv";
 import productRoutes from './routes/productRoutes.js'
+import {notFound, errorHandler} from  './middleware/errorMiddleware.js'
 
 dotenv.config();
 
@@ -15,6 +16,9 @@ app.get("/", (req, res) => {
 });
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
