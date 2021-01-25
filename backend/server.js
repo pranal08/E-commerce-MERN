56c04dce0001f134a2b61b97d14ea390237fe740
Pranal PradeepKumar Kuttappan <pranalpk8@gmail.com>
2021-01-25 02:49:13 -0500

    Changes in the backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-import path from 'path'
+import path from "path";
 import express from "express";
-import morgan from 'morgan';
+import morgan from "morgan";
 import connectDB from "./config/db.js";
 import products from "./data/products.js";
 import dotenv from "dotenv";
@@ -16,15 +16,11 @@ connectDB();
 
 const app = express();
 
-if(process.env.NODE_ENV === 'development'){
-  app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -35,8 +31,19 @@ app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
-const __dirname = path.resolve()
-app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
